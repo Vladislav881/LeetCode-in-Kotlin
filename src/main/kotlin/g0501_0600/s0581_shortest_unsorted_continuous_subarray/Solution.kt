145b13ceefff9dcf28d311b21832c23f44268747
Vladislav <efefef2021@bk.ru>
2024-01-03 02:17:02 +0300

    new solution 581 Time 89.29% Space 92.86%

--- src/main/kotlin/g0501_0600/s0581_shortest_unsorted_continuous_subarray/Solution.kt
+++ src/main/kotlin/g0501_0600/s0581_shortest_unsorted_continuous_subarray/Solution.kt
@@ -1,26 +1,27 @@
 package g0501_0600.s0581_shortest_unsorted_continuous_subarray
 
 // #Medium #Array #Sorting #Greedy #Two_Pointers #Stack #Monotonic_Stack
-// #2023_01_30_Time_246_ms_(100.00%)_Space_39.4_MB_(50.00%)
+// #2024_01_04_Time_233_ms_(89.29%)_Space_39.40_MB_(92.86%)
 
 class Solution {
     fun findUnsortedSubarray(nums: IntArray): Int {
-        var end = -2
-        var max = Int.MIN_VALUE
-        for (i in nums.indices) {
-            max = Math.max(max, nums[i])
-            if (nums[i] < max) {
-                end = i
+        fun findUnsortedSubarray(nums: IntArray): Int {
+            var end = -2
+            var max = Int.MIN_VALUE
+            var start = -1
+            var min = Int.MAX_VALUE
+            val lengg = nums.size - 1
+
+            for(i in nums.indices){
+                max = Math.max(max, nums[i])
+                min = Math.min(min, nums[lengg - i])
+                if (nums[i] < max){
+                    end = i
+                }
+                if (nums[lengg - i] > min){
+                    start = lengg - i
+                }
             }
+            return end - start + 1
         }
-        var start = -1
-        var min = Int.MAX_VALUE
-        for (i in nums.indices.reversed()) {
-            min = Math.min(min, nums[i])
-            if (nums[i] > min) {
-                start = i
-            }
-        }
-        return end - start + 1
-    }
 }
